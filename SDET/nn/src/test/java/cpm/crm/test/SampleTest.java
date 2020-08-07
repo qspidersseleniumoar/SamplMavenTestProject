10fdbd1d730f96febb10ab0cd2f627460f0b3eaa
SR Studio <SR Studio@DESKTOP-930H2SV.lan>
2020-08-07 12:34:44 +0530

    pusht framwork

---
+++ SDET/nn/src/test/java/cpm/crm/test/SampleTest.java
@@ -0,0 +1,28 @@
+package cpm.crm.test;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void createContact() {
+		System.out.println("execute createContact");
+	}
+	
+	@Test
+	public void modifyContact() {
+		System.out.println("execute modifyContact");
+	}
+	
+	
+	@Test
+	public void searchContact() {
+		System.out.println("execute searchContact");
+	}
+	
+	@Test
+	public void deleteContact() {
+		System.out.println("execute deleteContact");
+	}
+
+}
